a1d807fd8d404f55861c43f85cb92830eede67a5
Cakiste <58522579+Cakiste@users.noreply.github.com>
2021-03-31 11:01:36 +0200

    PageJournal

--- frontend/journal.php
+++ frontend/journal.php
@@ -15,8 +15,86 @@
     else
         require_once("error.php");
 ?>
-    <h2>Du contenu</h2> 
-                <p>Toujours + de contenu</p>
+<script type="text/javascript" charset="utf8" src="js/datatables.js"></script>
+    <table>
+            <tr>
+                <td>
+                    <iframe name="iframe" style="display:none;"></iframe>
+                    <form id="formJ" method="GET" action="../backend/addAliment.php" autocomplete="off" target="iframe"> 
+                        <div>
+                            <label>Plat :</label><label class="validation-error hide" id="nomValidationError">Veuillez remplir toutes les informations s'il vous plaît</label>
+                            <select name="nom" id="nom" form="formJ">  
+                            </select>
+                        </div>
+                        <p>
+                            <label>Quantité (en g) :</label>
+                            <input type="float" name="quantite" id="quantite">
+                        </p>
+                        <p>
+                            <label>Date du repas :</label>
+                            <input type="date" name="date" id="date">
+                        </p>
+                        <div class="bouton-du-form">
+                            <input id="formbutton" type="submit" value="Submit">
+                        </div>
+                        </form>
+                        <?php ?>
+                </td>
+                <td>
+                    <table class="liste" id="listerepas">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Nom</th>
+                                <th>Quantité (en g)</th>
+                                <th>Date</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <script>$(document).ready( function () {
+                    var table =$('#listerepas').DataTable({
+                        'ajax':{
+                            "url":"../backend/getJournal.php",
+                            "dataSrc":""
+                        },
+                        "columnDefs": [ {
+                            "targets": -1,
+                            "data": null,
+                            "defaultContent": "<button class=\"delete\">Delete</button><button class=\"update\">Update</button>" //https://datatables.net/examples/ajax/null_data_source.html
+                        } ]
+                    });
+                    $("#listealiment tbody").on('click','.delete',function() {
+                        //delAliment(this.closest('tr').cells[0].innerHTML);
+                    });
+                    $("#listealiment tbody").on('click','.update', function() {
+                        //onEdit(this);
+                    });
+                    $(".apport").on('submit', function(e){
+                        e.preventDefault();
+                        //onFormSubmit();
+                    });
+                    $(function(){
+
+                        var items="";
+                        $.getJSON("../backend/getAliment?id=1",function(data){
+
+                        $.each(data,function(index,item) 
+                        {
+                            items+="<option value='"+item.id_aliment+"'>"+item.nom+"</option>";
+                        });
+                        $("#nom").html(items); 
+                        });
+
+                    });
+                } )</script>
+        <script src="js/crud.js"></script>
 </section>
     <?php
         require_once("footer.php");
